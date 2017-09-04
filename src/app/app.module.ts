a40deae01f8b591175dce49fdfd08331ff4d6ce8
Bastien Deroo <bderoo@financeactive.com>
2017-09-04 10:17:46 +0200

    #3 Router Outlet set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { HeaderComponent } from './shared/header/header.component';
+
+ const appRoutes: Routes = [
+  { path: 'crisis-center', component: HeaderComponent },
+//   { path: 'hero/:id',      component: HeroDetailComponent },
+//   {
+//     path: 'heroes',
+//     component: HeroListComponent,
+//     data: { title: 'Heroes List' }
+//   },
+//   { path: '',
+//     redirectTo: '/heroes',
+//     pathMatch: 'full'
+//   },
+//   { path: '**', component: PageNotFoundComponent }
+ ];
 
 @NgModule({
   declarations: [
@@ -12,6 +28,10 @@ import { HeaderComponent } from './shared/header/header.component';
     HeaderComponent
   ],
   imports: [
+    RouterModule.forRoot(
+  appRoutes,
+  { enableTracing: true } // <-- debugging purposes only
+),
     BrowserModule
   ],
   providers: [],
